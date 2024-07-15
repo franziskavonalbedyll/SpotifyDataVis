40ba5036b87044065b8f79a120f70759a90c14ad
Franzi <92818644+franziskavonalbedyll@users.noreply.github.com>
2024-07-15 14:13:14 +0200

    feat: bound color scale ranges together

--- app.py
+++ app.py
@@ -69,7 +69,6 @@ app.layout = html.Div(style={'height': '100vh', 'width': '100vw', 'display': 'fl
                           ], style={'width': '100%', 'padding': '0px', 'flex': '0 1 auto'})
                       ])
 
-
 @app.callback(
     [Output('choropleth-map-1', 'figure'),
      Output('choropleth-map-2', 'figure'),
@@ -83,25 +82,46 @@ app.layout = html.Div(style={'height': '100vh', 'width': '100vw', 'display': 'fl
      Input('audio-feature-dropdown-2', 'value'),
      Input('date-slider', 'value')]
 )
-def update_maps(selected_year_1, selected_audio_feature_1, selected_year_2, selected_audio_feature_2,
-                selected_date_idx):
+def update_maps(selected_year_1, selected_audio_feature_1, selected_year_2, selected_audio_feature_2, selected_date_idx):
     selected_date = dates[selected_date_idx]
 
-    # Filter data for the choropleth maps
-    filtered_df_1 = df[(df['year'] == selected_year_1) & (df['date'] == selected_date)]
-    filtered_df_2 = df[(df['year'] == selected_year_2) & (df['date'] == selected_date)]
+    # Filter data for the selected years
+    year_data_1 = df[df['year'] == selected_year_1]
+    year_data_2 = df[df['year'] == selected_year_2]
 
+    # Calculate color scales based on the selected audio features and years
+    if selected_audio_feature_1 == selected_audio_feature_2:
+        feature_range = [
+            min(year_data_1[selected_audio_feature_1].min(), year_data_2[selected_audio_feature_1].min()),
+            max(year_data_1[selected_audio_feature_1].max(), year_data_2[selected_audio_feature_1].max())
+        ]
+        color_scale = px.colors.sequential.Viridis
+    else:
+        feature_range_1 = [year_data_1[selected_audio_feature_1].min(), year_data_1[selected_audio_feature_1].max()]
+        feature_range_2 = [year_data_2[selected_audio_feature_2].min(), year_data_2[selected_audio_feature_2].max()]
+        color_scale_1 = px.colors.sequential.Viridis
+        color_scale_2 = px.colors.sequential.Cividis
+
+    # Filter data for the selected dates
+    filtered_df_1 = year_data_1[year_data_1['date'] == selected_date]
+    filtered_df_2 = year_data_2[year_data_2['date'] == selected_date]
+
+    # Create choropleth maps
     fig1 = px.choropleth(filtered_df_1, locations="region",
                          locationmode='country names', color=selected_audio_feature_1,
                          hover_name="region", title=f"Data for {selected_year_1}-{selected_date}",
-                         color_continuous_scale=px.colors.sequential.Viridis,
-                         hover_data={'date': True, selected_audio_feature_1: True})
+                         color_continuous_scale=color_scale if selected_audio_feature_1 == selected_audio_feature_2 else color_scale_1,
+                         hover_data={'date': True, selected_audio_feature_1: True},
+                         range_color=feature_range if selected_audio_feature_1 == selected_audio_feature_2 else feature_range_1)
+    fig1.update_geos(scope='europe')
 
     fig2 = px.choropleth(filtered_df_2, locations="region",
                          locationmode='country names', color=selected_audio_feature_2,
                          hover_name="region", title=f"Data for {selected_year_2}-{selected_date}",
-                         color_continuous_scale=px.colors.sequential.Viridis,
-                         hover_data={'date': True, selected_audio_feature_2: True})
+                         color_continuous_scale=color_scale if selected_audio_feature_1 == selected_audio_feature_2 else color_scale_2,
+                         hover_data={'date': True, selected_audio_feature_2: True},
+                         range_color=feature_range if selected_audio_feature_1 == selected_audio_feature_2 else feature_range_2)
+    fig2.update_geos(scope='europe')
 
     fig1.update_layout(
         title_x=0.5,
@@ -109,7 +129,6 @@ def update_maps(selected_year_1, selected_audio_feature_1, selected_year_2, sele
         coloraxis_showscale=True,
         title_font=dict(family="Circular Black", size=16),
         margin=dict(l=0, r=0, t=30, b=0),
-        # gleiche Ränder
     )
 
     fig2.update_layout(
@@ -121,50 +140,51 @@ def update_maps(selected_year_1, selected_audio_feature_1, selected_year_2, sele
         margin=dict(l=0, r=0, t=30, b=0),
     )
 
-    # Filter and aggregate data for heatmaps
-    heatmap_data_1 = df[df['year'] == selected_year_1]
-    heatmap_data_2 = df[df['year'] == selected_year_2]
+    # Prepare data for heatmaps
+    heatmap_data_1 = year_data_1.copy()
+    heatmap_data_2 = year_data_2.copy()
 
-    # Append year to date to form datetime
     heatmap_data_1['datetime'] = pd.to_datetime(heatmap_data_1['year'].astype(str) + '-' + heatmap_data_1['date'], format='%Y-%m-%d')
     heatmap_data_2['datetime'] = pd.to_datetime(heatmap_data_2['year'].astype(str) + '-' + heatmap_data_2['date'], format='%Y-%m-%d')
 
     heatmap_data_1 = heatmap_data_1.groupby('datetime')[selected_audio_feature_1].mean().reset_index()
     heatmap_data_2 = heatmap_data_2.groupby('datetime')[selected_audio_feature_2].mean().reset_index()
 
-    # normalize data for heatmap - Makes it easier to compare the audio features, but "distorts" the actual values
+    # Normalize data for heatmap
     heatmap_data_1[selected_audio_feature_1] = (heatmap_data_1[selected_audio_feature_1] - heatmap_data_1[selected_audio_feature_1].min()) / (heatmap_data_1[selected_audio_feature_1].max() - heatmap_data_1[selected_audio_feature_1].min())
-
     heatmap_data_2[selected_audio_feature_2] = (heatmap_data_2[selected_audio_feature_2] - heatmap_data_2[selected_audio_feature_2].min()) / (heatmap_data_2[selected_audio_feature_2].max() - heatmap_data_2[selected_audio_feature_2].min())
 
     heatmap_fig_1 = go.Figure(data=go.Heatmap(
         z=heatmap_data_1[selected_audio_feature_1],
         x=heatmap_data_1['datetime'],
-        y=[selected_audio_feature_1] * len(heatmap_data_1),  # y-axis to display the audio feature name
-        colorscale='Viridis',
-        showscale=False,  # Remove colorscale
+        y=[selected_audio_feature_1] * len(heatmap_data_1),
+        colorscale=color_scale if selected_audio_feature_1 == selected_audio_feature_2 else color_scale_1,
+        zmin=feature_range[0] if selected_audio_feature_1 == selected_audio_feature_2 else feature_range_1[0],
+        zmax=feature_range[1] if selected_audio_feature_1 == selected_audio_feature_2 else feature_range_1[1],
+        showscale=False,
     ))
     heatmap_fig_1.add_vline(x=heatmap_data_1['datetime'].iloc[selected_date_idx], line_width=3, line_dash="dash", line_color="red")
     heatmap_fig_1.update_layout(
-        yaxis=dict(visible=False),  # Hide y-axis
-        margin=dict(l=0, r=25, t=0, b=0)  # Remove margins
+        yaxis=dict(visible=False),
+        margin=dict(l=0, r=25, t=0, b=0)
     )
 
     heatmap_fig_2 = go.Figure(data=go.Heatmap(
         z=heatmap_data_2[selected_audio_feature_2],
         x=heatmap_data_2['datetime'],
-        y=[selected_audio_feature_2] * len(heatmap_data_2),  # y-axis to display the audio feature name
-        colorscale='Viridis',
-        showscale=False,  # Remove colorscale
+        y=[selected_audio_feature_2] * len(heatmap_data_2),
+        colorscale=color_scale if selected_audio_feature_1 == selected_audio_feature_2 else color_scale_2,
+        zmin=feature_range[0] if selected_audio_feature_1 == selected_audio_feature_2 else feature_range_2[0],
+        zmax=feature_range[1] if selected_audio_feature_1 == selected_audio_feature_2 else feature_range_2[1],
+        showscale=False,
     ))
     heatmap_fig_2.add_vline(x=heatmap_data_2['datetime'].iloc[selected_date_idx], line_width=3, line_dash="dash", line_color="red")
     heatmap_fig_2.update_layout(
-        yaxis=dict(visible=False),  # Hide y-axis
-        margin=dict(l=0, r=25, t=0, b=0)  # Remove margins
+        yaxis=dict(visible=False),
+        margin=dict(l=0, r=25, t=0, b=0)
     )
 
     return fig1, fig2, heatmap_fig_1, heatmap_fig_2, {'height': '50vh', 'width': '100%', 'display': 'block'}, {'height': '50vh', 'width': '100%', 'display': 'block'}
-
 
 if __name__ == '__main__':
     app.run_server(debug=True)
