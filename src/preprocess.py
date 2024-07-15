8b1b65c9dbe2f45839ff6730e9fd676581c87dfb
Franzi <92818644+franziskavonalbedyll@users.noreply.github.com>
2024-07-15 13:52:13 +0200

    filter: only keep european countries

--- src/preprocess.py
+++ src/preprocess.py
@@ -9,8 +9,31 @@ def _keep_years(df, years_to_keep) -> pd.DataFrame:
     df = df[df.date.dt.year.isin(years_to_keep)]
     return df.sort_values(by=['date'])
 
-def _merge_charts_with_audio_features(audio_features: pd.DataFrame, years_to_keep) -> None:
-    charts = pd.read_csv(CHARTS_PATH)
+def _keep_european_countries(df):
+    european_countries = ['Andorra', 'Austria',
+                          'Belgium', 'Bulgaria',
+                          'Czech Republic', 'Denmark',
+                          'Estonia', 'Finland',
+                          'France', 'Germany',
+                          'Greece', 'Hungary',
+                          'Iceland', 'Ireland',
+                          'Italy', 'Latvia',
+                          'Lithuania', 'Luxembourg',
+                          'Netherlands', 'Norway',
+                          'Poland', 'Portugal',
+                          'Romania', 'Russia',
+                          'Slovakia', 'Spain',
+                          'Sweden', 'Switzerland',
+                          'Turkey', 'Ukraine',
+                          'United Kingdom']
+
+    df = df[df['region'].isin(european_countries)]
+
+    return df
+
+
+
+def _merge_charts_with_audio_features(audio_features: pd.DataFrame, charts: pd.DataFrame, years_to_keep) -> None:
     charts['date'] = pd.to_datetime(charts['date'])
     charts = _keep_years(charts, years_to_keep)
 
@@ -39,9 +62,11 @@ def preprocess_data(include_years: int):
     output_path = 'data/preprocessed_data/preprocessed_data.csv'
     if not os.path.exists(output_path):
         input_data = pd.read_csv(AUDIO_FEATURES_AND_GENRE_PATH)
+        charts = pd.read_csv(CHARTS_PATH)
 
         # Remove preprocessing steps by commenting them out
-        preprocessed_df = _merge_charts_with_audio_features(input_data, include_years)
+        preprocessed_charts = _keep_european_countries(charts)
+        preprocessed_df = _merge_charts_with_audio_features(input_data, preprocessed_charts, include_years)
         preprocessed_df = _transform_dates(preprocessed_df)
         preprocessed_df = _aggregate_audio_feature(preprocessed_df)
         preprocessed_df = _round_audio_features(preprocessed_df)
