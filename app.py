9f64d46a50dc4f4575e622d3d5f27f4fda36a275
Franzi <92818644+franziskavonalbedyll@users.noreply.github.com>
2024-07-19 15:34:40 +0200

    refactor: change directory structure

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import dash
 import dash_bootstrap_components as dbc
-from layout import layout
-from callbacks import register_callbacks
+from src.app.layout import layout
+from src.app.callbacks import register_callbacks
 
 # Initialize Dash app
 app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
--- src/utils.py
+++ src/utils.py
@@ -3,6 +3,3 @@ from src.config import *
 
 def get_current_date_and_time():
     return datetime.datetime.now().strftime("%m.%d.%Y_%H.%M.%S")
-
-def year_to_filepath(year: int):
-    return f'{PREPROCCESSED_DATA_DIR}/{str(year)}.csv'
